487faa6c1209b0d69f7f8bbec1ed58cf746a9550
Zakhar Popov <86370030+fl1pcoin@users.noreply.github.com>
2025-01-12 00:35:49 +0300

    Change setup.py and pyproject.toml

--- readmegen/setup.py
+++ readmegen/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="readmegen",
     version="0.1.0",
-    packages=find_packages(),
+    packages=find_packages(include=["readmegen", "readmegen.*"]),
     package_data={
         "readmegen": ["generators/svg/shieldsio_icons.json",
                       "generators/svg/skill_icons.json",],
