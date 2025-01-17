16dda7170cff8cf93f25b8dd29f63ccdbd22f6a8
Zakhar Popov <86370030+fl1pcoin@users.noreply.github.com>
2025-01-18 00:46:43 +0300

    Add logging and move configs to package

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 import os
 import argparse
+import logging
 from readmeai.config.settings import ConfigLoader, GitSettings
 from readmeai.main import readme_generator
+
+for handler in logging.root.handlers[:]:
+    logging.root.removeHandler(handler)
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(message)s",
+)
 
 
 def main():
@@ -21,20 +30,30 @@ def main():
 
 
 def readme_agent(repo_url: str) -> None:
-    # Loading configurations and updating repo_url
-    config_loader = ConfigLoader(config_dir="OSA/config")
-    config_loader.config.git = GitSettings(repository=repo_url)
+    logging.info("Started generating README.md. "
+                 "Processing the repository: %s", repo_url)
 
-    # Path to save README.md
-    output_dir = os.path.join(os.getcwd(),
-                              "examples",
-                              config_loader.config.git.name,
-                              )
-    os.makedirs(output_dir, exist_ok=True)
-    file_to_save = os.path.join(output_dir, "README.md")
+    try:
+        # Loading configurations and updating repo_url
+        config_loader = ConfigLoader(config_dir="OSA/config")
+        config_loader.config.git = GitSettings(repository=repo_url)
 
-    # Generate README.md
-    readme_generator(config_loader, file_to_save)
+        # Path to save README.md
+        output_dir = os.path.join(os.getcwd(),
+                                  "examples",
+                                  config_loader.config.git.name,
+                                  )
+        os.makedirs(output_dir, exist_ok=True)
+        file_to_save = os.path.join(output_dir, "README.md")
+
+        # Generate README.md
+        readme_generator(config_loader, file_to_save)
+
+        logging.info("README.md successfully generated in folder: %s",
+                     output_dir)
+
+    except Exception as e:
+        logging.error("Error while generating: %s", e, exc_info=True)
 
 
 if __name__ == "__main__":
