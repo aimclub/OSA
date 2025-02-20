fa443d4064b7a09f1fcf6161316b42059ad2ffe2
Tendo1904 <71323590+Tendo1904@users.noreply.github.com>
2025-02-20 17:17:46 +0300

    docstring generation to a function with already existing docstring bugfix

--- OSA/osatreesitter/docgen.py
+++ OSA/osatreesitter/docgen.py
@@ -305,15 +305,16 @@ class DocGen(object):
                             )
                 if item["type"] == "function":
                     func_details = item["details"]
-                    logging.info(
-                        f"Generating docstring for a function: {func_details['method_name']} at {filename}"
-                    )
-                    generated_docstring = self.generate_method_documentation(
-                        func_details
-                    )
-                    source_code = self.insert_docstring_in_code(
-                        source_code, func_details, generated_docstring
-                    )
+                    if func_details["docstring"] == None:
+                        logging.info(
+                            f"Generating docstring for a function: {func_details['method_name']} at {filename}"
+                        )
+                        generated_docstring = self.generate_method_documentation(
+                            func_details
+                        )
+                        source_code = self.insert_docstring_in_code(
+                            source_code, func_details, generated_docstring
+                        )
 
             for item in structure:
                 if item["type"] == "class" and item["docstring"] == None:
