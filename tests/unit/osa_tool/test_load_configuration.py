d62190e1cac87d752f78120ec087245f3f1404a5
fl1pcoin <popov.zakharqwe123@gmail.com>
2025-03-29 16:01:32 +0300

    Update test_load_configuration.py

--- tests/unit/osa_tool/test_load_configuration.py
+++ tests/unit/osa_tool/test_load_configuration.py
@@ -1,35 +1,33 @@
+import pytest
 from unittest.mock import patch, MagicMock
 
 from osa_tool.run import load_configuration
 
 
+@pytest.mark.parametrize(
+    "repo_url, api, api_url, model, article,"
+    "expected_repo, expected_api, expected_url, expected_model",
+    [
+        ("https://github.com/example/repo", "openai", "https://api.openai.com", "gpt-test", None,
+         "https://github.com/example/repo", "openai", "https://api.openai.com", "gpt-test"),
+        ("https://github.com/example/repo", "openai", "https://api.openai.com", "gpt-test-article", "article.pdf",
+         "https://github.com/example/repo", "openai", "https://api.openai.com", "gpt-test-article")
+    ]
+)
+@patch("osa_tool.utils.osa_project_root", return_value="/mock/project/root")
 @patch("osa_tool.readmeai.config.settings.ConfigLoader", autospec=True)
-@patch("osa_tool.utils.osa_project_root", return_value="/mock/project/root")
-def test_load_configuration_without_article(mock_project_root, mock_loader):
-    # Arrange
-    mock_config = MagicMock()
-    mock_loader.return_value = mock_config
-    # Act
-    config = load_configuration("https://github.com/example/repo", "openai",
-                                "https://api.openai.com", "gpt-4", None)
-    # Assert
-    assert config.config.git.repository == "https://github.com/example/repo"
-    assert config.config.llm.api == "openai"
-    assert config.config.llm.url == "https://api.openai.com"
-    assert config.config.llm.model == "gpt-4"
-
-
 @patch("osa_tool.readmeai.readmegen_article.config.settings.ArticleConfigLoader", autospec=True)
-@patch("osa_tool.utils.osa_project_root", return_value="/mock/project/root")
-def test_load_configuration_with_article(mock_project_root, mock_loader):
+def test_load_configuration(mock_article_loader, mock_config_loader, mock_project_root,
+                            repo_url, api, api_url, model, article,
+                            expected_repo, expected_api, expected_url, expected_model):
     # Arrange
     mock_config = MagicMock()
-    mock_loader.return_value = mock_config
+    mock_config_loader.return_value = mock_config
+    mock_article_loader.return_value = mock_config
     # Act
-    config = load_configuration("https://github.com/example/repo", "openai",
-                                "https://api.openai.com", "gpt-4", "article.pdf")
+    config = load_configuration(repo_url, api, api_url, model, article)
     # Assert
-    assert config.config.git.repository == "https://github.com/example/repo"
-    assert config.config.llm.api == "openai"
-    assert config.config.llm.url == "https://api.openai.com"
-    assert config.config.llm.model == "gpt-4"
+    assert config.config.git.repository == expected_repo
+    assert config.config.llm.api == expected_api
+    assert config.config.llm.url == expected_url
+    assert config.config.llm.model == expected_model
