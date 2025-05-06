2684babcaf3beda8898e14f217978b2b8fedfd4d
DRMPN <mr.gorden.smit@gmail.com>
2025-05-06 23:49:33 +0300

    feat: add AboutGenerator class

--- osa_tool/run.py
+++ osa_tool/run.py
@@ -1,7 +1,9 @@
 import os
 from typing import List
+
 import tomli
 
+from osa_tool.aboutgen.about_generator import AboutGenerator
 from osa_tool.analytics.report_maker import ReportGenerator
 from osa_tool.analytics.sourcerank import SourceRank
 from osa_tool.arguments_parser import get_cli_args
@@ -13,11 +15,7 @@ from osa_tool.osatreesitter.docgen import DocGen
 from osa_tool.osatreesitter.osa_treesitter import OSA_TreeSitter
 from osa_tool.readmegen.readme_core import readme_agent
 from osa_tool.translation.dir_translator import DirectoryTranslator
-from osa_tool.utils import (
-    delete_repository,
-    logger,
-    parse_folder_name
-)
+from osa_tool.utils import delete_repository, logger, parse_folder_name
 
 
 def main():
@@ -112,6 +110,10 @@ def main():
 
         # Readme generation
         readme_agent(config, article)
+
+        # About section generation
+        about_gen = AboutGenerator(config, sourcerank)
+        about_data = about_gen.generate_about_section()
 
         # Generate GitHub workflows
         if generate_workflows:
