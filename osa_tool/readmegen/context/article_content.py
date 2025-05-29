fcf22b906b7c47d0bc4cac21d12d83a78c516b7f
Nikolay Nikitin <nicl.nno@yandex.ru>
2025-05-29 14:41:26 +0300

    MacOS workaround (#164)
    * Macos workaround
    
    * Req upd
    
    * Remove ap.Document type annotation from extract_table_bboxes
    
    ---------
    
    Co-authored-by: Вадим Ахмеров <vadimspring@mail.ru>

--- osa_tool/readmegen/context/article_content.py
+++ osa_tool/readmegen/context/article_content.py
@@ -1,7 +1,11 @@
 import os
 from pathlib import Path
 
-import aspose.pdf as ap
+try:
+    import aspose.pdf as ap
+except Exception as ex:
+    print(f"Can not use aspose lib: {ex}. Please note that it can be MacOS-specific issue.")
+
 from pdfminer.high_level import extract_pages
 from pdfminer.layout import LTLine, LTTextContainer
 
@@ -55,7 +59,7 @@ class PdfParser:
 
     @staticmethod
     def extract_table_bboxes(
-        doc: ap.Document
+        doc
     ) -> dict[int, list[tuple[float, float, float, float]]]:
         """Extract standard table bounding boxes using Aspose."""
         table_bboxes: dict[int, list[tuple[float, float, float, float]]] = {}
