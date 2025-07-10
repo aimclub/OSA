37dfd53074770a00779a2609d210db4e20fa312b
fl1pcoin <popov.zakharqwe123@gmail.com>
2025-07-11 01:22:51 +0300

    changes after review

--- osa_tool/run.py
+++ osa_tool/run.py
@@ -71,7 +71,7 @@ def main():
             github_agent.create_and_checkout_branch()
 
         # .ipynb to .py convertion
-        if not plan.get("convert_notebooks") is None:
+        if plan["convert_notebooks"] is not None:
             rich_section("Jupyter notebooks convertion")
             convert_notebooks(args.repository, plan.get("convert_notebooks"))
 
