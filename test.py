142386755cff224fedccbb0c4c94e74c15ddd0cd
DRMPN <mr.gorden.smit@gmail.com>
2025-10-01 13:23:37 +0300

    feat: add all repo's urls

--- test.py
+++ test.py
@@ -63,18 +63,109 @@ def run(repos, folder):
         print(f"Elapsed time: {total}")
         print(f"{r}\t{total}")
 
-    print("Waiting 5 minutes...")
-    time.sleep(60 * 5)
+    # print("Waiting 5 minutes...")
+    # time.sleep(60 * 5)
     print()
 
 
 if __name__ == "__main__":
+
+    # repos = [
+    #     "https://github.com/Jarvis-Yu/Dottore-Genius-Invokation-TCG-Simulator",
+    #     "https://github.com/Josh-XT/pytube2",
+    #     "https://github.com/sedrew/petpptx",
+    #     "https://github.com/8451/labrea",
+    #     "https://github.com/Psychevus/cryptography-suite",
+    # ]
+    # run(repos, "random_1_new")
+
+    # repos = [
+    #     "https://github.com/exdatic/vectorspace",
+    #     "https://github.com/ryan95f/yamlator",
+    #     "https://github.com/Mongorunway/mongorunway",
+    #     "https://github.com/atomic6-org/ghg",
+    #     "https://github.com/lestex/pygithubactions",
+    # ]
+    # run(repos, "random_1_old")
+
+    # repos = [
+    #     "https://github.com/Universite-Gustave-Eiffel/acoustic-toolbox",
+    #     "https://github.com/NVIDIA/cloudai",
+    #     "https://github.com/hellosign/dropbox-sign-python",
+    #     "https://github.com/medkit-lib/medkit",
+    #     "https://github.com/sieve-community/pytube",
+    # ]
+    # run(repos, "random_2_new")
+
+    # repos = [
+    #     "https://github.com/foozzi/Dali",
+    #     "https://github.com/aerleon/aerleon",
+    #     "https://github.com/GPLgithub/sg-otio",
+    #     "https://github.com/MaxAtkinson/tilted",
+    #     "https://github.com/scottbarnes/reconcile",
+    # ]
+    # run(repos, "random_2_old")
+
+    # repos = [
+    #     "https://github.com/mrlooi/latex2json",
+    #     "https://github.com/DLR-KI/scan",
+    #     "https://github.com/Onix-Systems/python-pin-payments",
+    #     "https://github.com/WildDIC/pytube",
+    #     "https://github.com/loonghao/transx",
+    # ]
+    # run(repos, "random_3_new")
+
+    # repos = [
+    #     "https://github.com/hanjinliu/acryo",
+    #     "https://github.com/front-matter/commonmeta-py",
+    #     "https://github.com/OpenVoiceOS/ovos-lingua-franca",
+    #     "https://github.com/BSpendlove/pykeadhcp",
+    #     "https://github.com/ImperialCollegeLondon/wsi",
+    # ]
+    # run(repos, "random_3_old")
+
+    # repos = [
+    #     "https://github.com/zabbix/python-zabbix-utils",
+    #     "https://github.com/weareprestatech/hotpdf",
+    #     "https://github.com/awslabs/agent-evaluation",
+    #     "https://github.com/dreadnode/rigging",
+    #     "https://github.com/pgorecki/lato",
+    # ]
+    # run(repos, "random_4_new")
+
+    # repos = [
+    #     "https://github.com/citrusvanilla/tinyflux",
+    #     "https://github.com/dylanljones/pyrekordbox",
+    #     "https://github.com/alexmon1989/russia_ddos",
+    #     "https://github.com/flakeheaven/flakeheaven",
+    #     "https://github.com/haiiliin/abqpy",
+    # ]
+    # run(repos, "random_4_old")
+
+    # repos = [
+    #     "https://github.com/serengil/LightPHE",
+    #     "https://github.com/the-siesta-group/edfio",
+    #     "https://github.com/xvnpw/ai-security-analyzer",
+    #     "https://github.com/capitec/dsp-decision-engine",
+    #     "https://github.com/Citi/pargraph",
+    # ]
+    # run(repos, "random_5_new")
+
+    # repos = [
+    #     "https://github.com/saltstack/relenv",
+    #     "https://github.com/proroklab/popgym",
+    #     "https://github.com/probabilists/zuko",
+    #     "https://github.com/znstrider/plottable",
+    #     "https://github.com/fox-it/dissect.cstruct",
+    # ]
+    # run(repos, "random_5_old")
+
     repos = [
-        "https://github.com/psf/requests",
-        # "https://github.com/pallets/flask",
-        # "https://github.com/mwaskom/seaborn",
+        # "https://github.com/psf/requests",
+        "https://github.com/pallets/flask",
+        "https://github.com/mwaskom/seaborn",
         # "https://github.com/matplotlib/matplotlib",
-        # "https://github.com/paul-gauthier/aider",
+        "https://github.com/paul-gauthier/aider",
     ]
     run(repos, "popular_1")
 
