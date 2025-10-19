a8c9f6e562df3a4d37b5baac2417533e9f62182f
DRMPN <mr.gorden.smit@gmail.com>
2025-10-19 23:22:12 +0300

    style: fix black lint

--- osa_tool/readmegen/readme_core.py
+++ osa_tool/readmegen/readme_core.py
@@ -1,7 +1,7 @@
 import os
 
-from osa_tool.config.settings import ConfigLoader
 from osa_tool.analytics.metadata import RepositoryMetadata
+from osa_tool.config.settings import ConfigLoader
 from osa_tool.readmegen.generator.builder import MarkdownBuilder
 from osa_tool.readmegen.generator.builder_article import MarkdownBuilderArticle
 from osa_tool.readmegen.models.llm_service import LLMClient
@@ -9,7 +9,9 @@ from osa_tool.readmegen.utils import remove_extra_blank_lines, save_sections
 from osa_tool.utils import logger, parse_folder_name
 
 
-def readme_agent(config_loader: ConfigLoader, article: str | None, refine_readme: bool, metadata: RepositoryMetadata) -> None:
+def readme_agent(
+    config_loader: ConfigLoader, article: str | None, refine_readme: bool, metadata: RepositoryMetadata
+) -> None:
     """Generates a README.md file for the specified GitHub repository.
 
     Args:
