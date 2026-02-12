39c5b7b7ed41e45913c440c4e3933d1bba662779
Azat Akhmetyanov <a.r.akhmetyanov@gmail.com>
2026-02-12 15:27:51 +0300

    code analysis

--- osa_tool/validation/code_analyzer.py
+++ osa_tool/validation/code_analyzer.py
@@ -23,7 +23,7 @@ class CodeAnalyzer:
     and sending code content to a model for analysis.
     """
 
-    SOURCEFILE_EXTENSIONS_LIST = "py", "c", "cpp", "kt", "java", "js", "ts", "go", "rb", "rs", "swift", "cs", "php"
+    SOURCEFILE_EXTENSIONS_LIST = "py", "c", "cpp", "kt", "java", "js", "ts", "go", "rb", "rs", "swift", "cs", "php", "cmake", "h", "hpp"
     IGNORE_LIST = (
         "__init__.py",
         "setup.py",
