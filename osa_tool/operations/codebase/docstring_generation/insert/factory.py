79a9e06ccd988f2b5d8a228da0f8c0fc9a208b53
Tendo1904 <tema.protopopov.91@mail.ru>
2026-06-02 21:59:31 +0300

    minor black fixes

--- osa_tool/operations/codebase/docstring_generation/insert/factory.py
+++ osa_tool/operations/codebase/docstring_generation/insert/factory.py
@@ -1,6 +1,7 @@
 from osa_tool.operations.codebase.docstring_generation.insert.python_augmentor import PythonAugmentor
 from osa_tool.operations.codebase.docstring_generation.insert.ts_js_augmentor import TSJSAugmentor
 
+
 class AugmentorFactory:
 
     @staticmethod
