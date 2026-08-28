b84afaa33d70334655e0ca4f0ec21405ae5effc7
DRMPN <mr.gorden.smit@gmail.com>
2026-08-28 10:39:44 +0300

    chore: fix black formatting

--- tests/unit/test_optional_dependency_boundaries.py
+++ tests/unit/test_optional_dependency_boundaries.py
@@ -16,7 +16,8 @@ _OPTIONAL_MODULES = {
 
 
 def test_core_imports_do_not_require_optional_feature_dependencies():
-    script = textwrap.dedent(f"""
+    script = textwrap.dedent(
+        f"""
         import importlib.abc
         import sys
 
@@ -31,7 +32,8 @@ def test_core_imports_do_not_require_optional_feature_dependencies():
         import osa_tool.run
         import osa_tool.operations.operations_catalog
         import osa_tool.operations.analysis.paper_claims
-        """)
+        """
+    )
     project_root = Path(__file__).resolve().parents[2]
 
     result = subprocess.run(
